155675ed3416a7463f9b95bdfc093f8ea847d77b
nametake <nametake.kyarabuki@gmail.com>
2014-03-29 02:44:16 +0900

    デザイン完全完成

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         //スライドカラー
         slidesColor: ['#cd9cc1', '#ffd194', '#fff9a2', '#ffd194', '#f9a383', '#f8a5a6'],
         //アンカー アドレスの後のにつく文字
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5thpage'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5thpage', '6thpage'],
         //イベント処理
         //indexが現在のページ．directionがどちらに動いたか．
         scrollingSpeed: 800,
